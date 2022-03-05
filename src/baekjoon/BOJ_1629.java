88cd99a30db33949c82cbe6563066ca3dfb37d88
GetMapping <eonju.dev@gmail.com>
2022-03-05 21:08:26 +0900

    solve: BOJ_1629

--- src/baekjoon/BOJ_1629.java
+++ src/baekjoon/BOJ_1629.java
@@ -6,14 +6,34 @@ import java.io.InputStreamReader;
 
 public class BOJ_1629 {
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            String[] input = bufferedReader.readLine().split(" ");
-            double number = Double.parseDouble(input[0]);
-            double pow = Double.parseDouble(input[1]);
-            int divide = Integer.parseInt(input[2]);
-            long answer = ((long) Math.pow(number, pow)) % divide;
-            System.out.println(answer);
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = bufferedReader.readLine().split(" ");
+        long number = Long.parseLong(input[0]);
+        long exponent = Long.parseLong(input[1]);
+        long divide = Integer.parseInt(input[2]);
+        long answer = cal(number, exponent, divide);
+        System.out.println(answer);
+    }
+
+    public static long cal(long number, long exponent, long divide) {
+        if (exponent == 0) {
+            return 1;
+        }
+
+        if (exponent == 1) {
+            return number;
+        }
+
+        if (exponent % 2 == 0) {
+            long n = cal(number, exponent / 2, divide) % divide;
+
+            return (n * n) % divide;
+        } else {
+            long n = cal(number, exponent / 2, divide) % divide;
+
+            return (((n * n) % divide) * number) % divide;
         }
     }
+}
 
